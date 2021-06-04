60c146c160aad874f2e13cdd60aaacaeb7ee8e04
jackpetersen3 <peterja7@oregonstate.edu>
2021-06-04 10:50:04 -0700

    created tests for add and sub methods

--- calc.py
+++ calc.py
@@ -2,9 +2,10 @@
 calculator app using circle CI
 """
 
-def add(x,y):
+
+def add(x, y):
     return x + y
 
-def sub(x,y):
-    return x - y
 
+def sub(x, y):
+    return x - y
---
+++ test_calc.py
@@ -0,0 +1,12 @@
+"""
+Tests for calc app
+"""
+import calc
+
+
+class TestCalc:
+    def test_add(self):
+        assert calc.add(3,2) == 5
+    
+    def test_sub(self):
+        assert calc.sub(10,5) == 5
