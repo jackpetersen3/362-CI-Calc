d8f30a598500f04587e87bce473c8b1c738a82dc
jackpetersen3 <peterja7@oregonstate.edu>
2021-06-04 11:16:20 -0700

    changed requirements error

--- test_calc.py
+++ test_calc.py
@@ -6,7 +6,7 @@ import calc
 
 class TestCalc:
     def test_add(self):
-        assert calc.add(3,2) == 5
-    
+        assert calc.add(3, 2) == 5
+
     def test_sub(self):
-        assert calc.sub(10,5) == 5
+        assert calc.sub(10, 5) == 5
